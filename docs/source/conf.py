ef61b76fd522d834e91525d89bc1a7b66e8f9ef6
Mihails Delmans <md@host134-41.pet.private.cam.ac.uk>
2019-01-24 01:41:47 +0000

    Use MagickMock

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,11 +21,11 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
 
-import mock
+from unittest.mock import MagicMock
 
 MOCK_MODULES = ['matplotlib', 'matplotlib.pyplot', 'dolfin', 'mpi4py', 'mshr', 'ufl']
 for mod_name in MOCK_MODULES:
-    sys.modules[mod_name] = mock.Mock()
+    sys.modules[mod_name] = MagicMock
 
 # -- General configuration ------------------------------------------------
 
